c1e91a77b115c3c8d8eb20ed020622db962ff477
Dawood AlOthman <Dawood@dawoods-macbook.lan>
2019-09-01 22:03:00 +0300

    first commit

---
+++ petapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Pet(models.Model):
+	name = models.CharField(max_length=50)
+	age = models.IntegerField()
+	available = models.BooleanField(default=True)
+	image = models.ImageField(null=True, blank=True)
+	price = models.DecimalField(max_digits=7, decimal_places=3)
+
+	def __str__(self):
+		return self.name
